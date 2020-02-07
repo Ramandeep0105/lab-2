10c941beb1e8da8538e3d617120a5fde8b640be6
ramandeep0105 <ramanbalu902@gmail.com>
2020-02-06 21:36:20 -0500

    Add project files.

---
+++ lab-2/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System
+    ;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string details(string id)
+        {
+
+            string message = HttpUtility.HtmlEncode("you selected product:" + id);
+            return message;
+        }
+    }
+
+}
